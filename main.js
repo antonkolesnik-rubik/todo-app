21f1cbed82ae8ffcb96a88f0e24015a82a162a67
antonkolesnik <ant.kol0512@gmail.com>
2023-11-30 22:40:29 +0200

    design and jsonplaceholder

--- main.js
+++ main.js
@@ -1,117 +1,89 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const taskForm = document.getElementById('taskForm');
+    let taskList = document.getElementById('taskList');
+
+    taskForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        addTaskFromForm();
+    });
+
+    restoreTasksFromLocalStorage(); // Відновлення стану з Local Storage
+});
+
 function addTask(taskText) {
-    // Додавання завдання
-}
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
 
-
-function deleteTask(taskId) {
-    // Видалення завдання
-}
-
-
-function toggleTaskStatus(taskId) {
-    // Зміна статусу завдання
-}
-
-
-function saveTasksToLocalStorage(tasks) {
-    const tasksJSON = JSON.stringify(tasks);
-    localStorage.setItem('tasks', tasksJSON);
-}
-
-
-function loadTasksFromLocalStorage() {
-    const tasksJSON = localStorage.getItem('tasks');
-    return JSON.parse(tasksJSON) || [];
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const taskForm = document.getElementById('taskForm');
-    const taskList = document.getElementById('taskList');
-
-    taskForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        addTask();
+    // Додайте обробник подій для відмітки завдань як виконаних
+    taskItem.addEventListener('click', function() {
+        taskItem.classList.toggle('completed');
+        saveTasksToLocalStorage(); // Збереження стану в Local Storage
     });
 
-    function addTask() {
-        const taskInput = document.getElementById('taskInput').value;
-        if (taskInput.trim() !== '') {
-            const taskItem = document.createElement('li');
-            taskItem.textContent = taskInput;
-            taskList.appendChild(taskItem);
-        }
-        // Додайте код для очищення полів форми чи інших необхідних дій.
-    }
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const taskForm = document.getElementById('taskForm');
-    const taskList = document.getElementById('taskList');
-
-    taskForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        addTask();
-    });
-
-});
-
-
-function addTask() {
-
-    const taskInput = document.getElementById('taskInput').value;
-    if (taskInput.trim() !== '') {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = taskInput;
-        taskList.appendChild(taskItem);
-        saveTasksToLocalStorage();
-        taskForm.reset();
-    }
-
+    taskList.appendChild(taskItem);
+    saveTasksToLocalStorage(); // Збереження стану в Local Storage
 }
-    const taskInput = document.getElementById('taskInput').value;
-    if (taskInput.trim() !== '') {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = taskInput;
-        taskList.appendChild(taskItem);
-        saveTasksToLocalStorage();
-        taskForm.reset();
-    }
+
+function deleteTask(taskItem) {
+    taskItem.remove();
+    saveTasksToLocalStorage(); // Збереження стану в Local Storage
+}
+
+function toggleTaskStatus(taskItem) {
+    taskItem.classList.toggle('completed');
+    saveTasksToLocalStorage(); // Збереження стану в Local Storage
+}
 
 function saveTasksToLocalStorage() {
     const tasks = [];
     const taskItems = document.querySelectorAll('#taskList li');
-    
+
     taskItems.forEach(function(taskItem) {
-        tasks.push(taskItem.textContent);
+        tasks.push({
+            text: taskItem.textContent,
+            completed: taskItem.classList.contains('completed')
+        });
     });
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const taskForm = document.getElementById('taskForm');
-    const taskList = document.getElementById('taskList');
-
-    taskForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        addTask();
-    });
-
-    restoreTasksFromLocalStorage(); // Відновлення стану з Local Storage
-    // Інші події та функції, якщо потрібно
-});
-
-function restoreTasksFromLocalStorage() {
+function loadTasksFromLocalStorage() {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     tasks.forEach(function(task) {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = task;
-        taskList.appendChild(taskItem);
+        addTaskFromStorage(task.text, task.completed);
     });
+}
+
+function addTaskFromStorage(text, completed) {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = text;
+
+    if (completed) {
+        taskItem.classList.add('completed');
+    }
+
+    // Додайте обробник подій для відмітки завдань як виконаних
+    taskItem.addEventListener('click', function() {
+        taskItem.classList.toggle('completed');
+        saveTasksToLocalStorage(); // Збереження стану в Local Storage
+    });
+
+    taskList.appendChild(taskItem);
+}
+
+function addTaskFromForm() {
+    const taskInput = document.getElementById('taskInput').value;
+    if (taskInput.trim() !== '') {
+        addTask(taskInput);
+        taskForm.reset(); // Очищення полів форми
+    }
+}
+
+function restoreTasksFromLocalStorage() {
+    loadTasksFromLocalStorage();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -129,65 +101,33 @@ function deleteCompletedTasks() {
     const completedTasks = document.querySelectorAll('#taskList li.completed');
 
     completedTasks.forEach(function(task) {
-        task.remove();
+        deleteTask(task);
     });
-
-    saveTasksToLocalStorage(); // Збереження стану в Local Storage
 }
 
 function deleteAllTasks() {
     const allTasks = document.querySelectorAll('#taskList li');
 
     allTasks.forEach(function(task) {
-        task.remove();
+        deleteTask(task);
     });
-
-    saveTasksToLocalStorage(); // Збереження стану в Local Storage
 }
 
+const url = 'https://jsonplaceholder.typicode.com/todos';
+const limit = 10;
 
-function addTask() {
-    const taskInput = document.getElementById('taskInput').value;
-    if (taskInput.trim() !== '') {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = taskInput;
-
-        // Додайте обробник подій для відмітки завдань як виконаних
-        taskItem.addEventListener('click', function() {
-            taskItem.classList.toggle('completed');
-            saveTasksToLocalStorage(); // Збереження стану в Local Storage
-        });
-
-        taskList.appendChild(taskItem);
-        saveTasksToLocalStorage(); // Збереження стану в Local Storage
-        taskForm.reset(); // Очищення полів форми
+fetch(`${url}?_limit=${limit}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
     }
-}
-
-
-function restoreTasksFromLocalStorage() {
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-    tasks.forEach(function(task) {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = task;
-
-        // Додайте клас "completed", якщо завдання виконано
-        if (isTaskCompleted(task)) {
-            taskItem.classList.add('completed');
-        }
-
-        taskItem.addEventListener('click', function() {
-            taskItem.classList.toggle('completed');
-            saveTasksToLocalStorage(); // Збереження стану в Local Storage
-        });
-
-        taskList.appendChild(taskItem);
+    return response.json();
+  })
+  .then(data => {
+    data.forEach(task => {
+      addTask(task.title);
     });
-}
-
-function isTaskCompleted(taskText) {
-    // Ваш код для визначення, чи завдання виконане
-    // Наприклад, можна перевірити наявність спеціального маркера у тексті
-    return taskText.includes('(completed)');
-}
+  })
+  .catch(error => {
+    console.error('Error during fetch operation:', error);
+  });ch('https://jsonplaceholder.typicode.com/todos?_limit=10')
